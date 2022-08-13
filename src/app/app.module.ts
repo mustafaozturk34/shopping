81f398461ccbedd299fa87ed0562c27103976109
mustafaozturk34 <mustafa.ozturk3408@gmail.com>
2022-08-13 12:24:34 +0300

    Adding components, installing necessary packages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,31 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { BasketComponent } from './components/basket/basket.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { PaymentComponent } from './components/payment/payment.component';
+import { ProductComponent } from './components/product/product.component';
+import { ToastrModule } from 'ngx-toastr';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    BasketComponent,
+    NavbarComponent,
+    PaymentComponent,
+    ProductComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    ToastrModule.forRoot({
+      closeButton: true,
+      progressBar: true
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
